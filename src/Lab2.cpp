4fa2f05ccb60e665196063eaba24cc58c25fe215
jaakkovo <jaakko.voutilainen@metropolia.fi>
2016-09-07 13:07:52 +0300

    Lab-2 valmis

--- src/Lab2.cpp
+++ src/Lab2.cpp
@@ -14,7 +14,7 @@
 #include "semphr.h"
 #include <stdio.h>
 #include <string.h>
-
+#include <cstdlib>
 
 #if defined (__USE_LPCOPEN)
 #if defined(NO_BOARD_LIB)
@@ -25,15 +25,18 @@
 #endif
 
 #include <cr_section_macros.h>
+#define LONG_TIME 0xffff
 
 // TODO: insert other include files here
-SemaphoreHandle_t xMutex = xSemaphoreCreateMutex();
-SemaphoreHandle_t xBool = xSemaphoreCreateCounting(2, 1);
-SemaphoreHandle_t xBool2 = xSemaphoreCreateCounting(2, 1);
+SemaphoreHandle_t xMutex = NULL;
+SemaphoreHandle_t xBinary = NULL;
+QueueHandle_t xQueue1 = NULL;
+
 char word[61];
 char empty[61];
 int indexi = 0;
 char question[] = "?";
+unsigned long longVar = 10UL;
 
 /* Sets up system hardware */
 static void prvSetupHardware(void)
@@ -51,12 +54,13 @@ static void task1(void *pvParameters) {
 			xSemaphoreTake(xMutex, portMAX_DELAY);
 			{
 				DEBUGOUT("SW1 pressed");
+				xSemaphoreGive ( xMutex );
 				Board_LED_Set(0, true);
 			}
 			while (!Chip_GPIO_GetPinState(LPC_GPIO, 0, 17)) {
 			}
 			Board_LED_Set(0, false);
-			xSemaphoreGive ( xMutex );
+
 		}
 
 		//DEBUGOUT("Tick: %d \r\n", tickCnt);
@@ -74,12 +78,13 @@ static void task2(void *pvParameters) {
 			xSemaphoreTake(xMutex, portMAX_DELAY);
 			{
 				DEBUGOUT("SW2 pressed");
+				xSemaphoreGive ( xMutex );
 				Board_LED_Set(1, true);
 			}
 			while (!Chip_GPIO_GetPinState(LPC_GPIO, 1, 11)) {
 			}
 			Board_LED_Set(1, false);
-			xSemaphoreGive ( xMutex );
+
 		}
 
 		//DEBUGOUT("Tick: %d \r\n", tickCnt);
@@ -92,29 +97,27 @@ static void task2(void *pvParameters) {
 
 static void readChars(void *pvParameters) {
 	int character;
-	char buf[256];
+	char buf[2];
 
 	while (1) {
 		character = Board_UARTGetChar();
 
 		if (character != -1){
-			sprintf(buf, "%c", character);
-			Board_UARTPutSTR (buf);
-			xSemaphoreTake(xBool, portMAX_DELAY);
+			//sprintf(buf, "%c", character);
+			//Board_UARTPutSTR (buf);
+			xSemaphoreGive( xBinary );
 		}
 	}
-	vTaskDelay(configTICK_RATE_HZ / 12);
 }
 
 /* UART (or output) thread */
 static void receivedChars(void *pvParameters) {
 	while (1) {
-		if (uxSemaphoreGetCount(xBool) == 0)
+		if(xSemaphoreTake(xBinary, portMAX_DELAY) == pdPASS)
 		{
 			Board_LED_Set(1, true);
 			vTaskDelay(configTICK_RATE_HZ / 10);
 			Board_LED_Set(1, false);
-			xSemaphoreGive(xBool);
 		}
 	}
 }
@@ -126,6 +129,11 @@ static void readChars2(void *pvParameters) {
 	int isThere = 0;
 
 	while (1) {
+
+		if(xQueue1 == NULL){
+			xQueue1 = xQueueCreate( 10, sizeof( unsigned long ) );
+		}
+
 		character = Board_UARTGetChar();
 		isThere = 0;
 		if (character != -1){
@@ -138,10 +146,11 @@ static void readChars2(void *pvParameters) {
 				indexi++;
 			}else{
 				indexi = 0;
-				Board_UARTPutSTR ("\r\n");
-				Board_UARTPutSTR ("[You] ");
+				Board_UARTPutSTR ("\r[You] ");
 				Board_UARTPutSTR (word);
 				Board_UARTPutSTR ("\r\n");
+
+				vTaskDelay(configTICK_RATE_HZ / 12);
 
 				for (int i = 0; i<61; i++){
 					if (word[i] == '?'){
@@ -150,7 +159,9 @@ static void readChars2(void *pvParameters) {
 				}
 
 				if(isThere == 1){
-					xSemaphoreTake(xBool2, portMAX_DELAY);
+
+					xQueueSend( xQueue1, (void*)&longVar, ( TickType_t ) 10 );
+
 					isThere = 0;
 				}
 
@@ -165,12 +176,36 @@ static void readChars2(void *pvParameters) {
 /* UART (or output) thread */
 static void receivedChars2(void *pvParameters) {
 	while (1) {
-		if (uxSemaphoreGetCount(xBool2) == 0)
-		{
-			Board_UARTPutSTR ("[Oracle] Hmmm...");
-			vTaskDelay(configTICK_RATE_HZ * 3);
+		if (xQueue1 != NULL){
+			unsigned long *longVar2;
+			if( xQueueReceive( xQueue1, & (longVar2), ( TickType_t ) 10 ) )
+			{
+				Board_UARTPutSTR ("[Oracle] Hmmm...\r\n");
+				vTaskDelay(configTICK_RATE_HZ * 3);
 
-			xSemaphoreGive(xBool2);
+				int x = rand() % 4;
+
+				switch(x)
+				{
+				case 0:
+					Board_UARTPutSTR ("[Oracle] I don't know!\r\n");
+					break;
+
+				case 1:
+					Board_UARTPutSTR ("[Oracle] Thats a tricky one!\r\n");
+					break;
+
+				case 2:
+					Board_UARTPutSTR ("[Oracle] Ask someone else!\r\n");
+					break;
+
+				case 3:
+					Board_UARTPutSTR ("[Oracle] Go away!\r\n");
+					break;
+				}
+
+				vTaskDelay(configTICK_RATE_HZ);
+			}
 		}
 	}
 }
@@ -197,41 +232,46 @@ int main(void) {
 	Chip_IOCON_PinMuxSet(LPC_IOCON, 1, 9, IOCON_DIGMODE_EN);
 	Chip_GPIO_SetPinDIRInput(LPC_GPIO, 1, 9);
 
+
+	xMutex = xSemaphoreCreateMutex();
+	xBinary = xSemaphoreCreateBinary();
+	xQueue1 = NULL;
+
 	/* EXERCISE 1 */
 
-	//	/* LED1 toggle thread */
-	//	xTaskCreate(task1, "task1",
+	//		/* LED1 toggle thread */
+	//		xTaskCreate(task1, "task1",
+	//				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+	//				(TaskHandle_t *) NULL);
+	//
+	//		/* LED2 toggle thread */
+	//		xTaskCreate(task2, "task2",
+	//				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+	//				(TaskHandle_t *) NULL);
+
+	//	/* EXERCISE 2 */
+	//
+	/* UART output thread, simply counts seconds */
+	xTaskCreate(readChars, "readChars",
+			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+	/* UART output thread, simply counts seconds */
+	xTaskCreate(receivedChars, "receivedChars",
+			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
+			(TaskHandle_t *) NULL);
+
+	/* EXERCISE 3 */
+
+	//	/* UART output thread, simply counts seconds */
+	//	xTaskCreate(readChars2, "readChars2",
 	//			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 	//			(TaskHandle_t *) NULL);
 	//
-	//	/* LED2 toggle thread */
-	//	xTaskCreate(task2, "task2",
-	//			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
-	//			(TaskHandle_t *) NULL);
-
-	/* EXERCISE 2 */
-
 	//	/* UART output thread, simply counts seconds */
-	//	xTaskCreate(readChars, "readChars",
+	//	xTaskCreate(receivedChars2, "receivedChars2",
 	//			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
 	//			(TaskHandle_t *) NULL);
-	//
-	//	/* UART output thread, simply counts seconds */
-	//	xTaskCreate(receivedChars, "receivedChars",
-	//			configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
-	//			(TaskHandle_t *) NULL);
-
-	/* EXERCISE 2 */
-
-		/* UART output thread, simply counts seconds */
-		xTaskCreate(readChars2, "readChars2",
-				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
-				(TaskHandle_t *) NULL);
-
-//		/* UART output thread, simply counts seconds */
-//		xTaskCreate(receivedChars2, "receivedChars2",
-//				configMINIMAL_STACK_SIZE, NULL, (tskIDLE_PRIORITY + 1UL),
-//				(TaskHandle_t *) NULL);
 
 
 	/* Start the scheduler */
